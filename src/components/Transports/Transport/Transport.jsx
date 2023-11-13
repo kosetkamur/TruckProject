b4650f64b858d6b6ab2c061f3caba89f5bea9b4e
kosetkamur <nataliyamaxi@icloud.com>
2023-11-13 22:19:39 +0300

    new images

--- src/components/Transports/Transport/Transport.jsx
+++ src/components/Transports/Transport/Transport.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import icon from "../../../media/img/icon.svg";
+import icon2 from "../../../media/img/icon2.svg";
+import icon3 from "../../../media/img/icon3.svg";
 import "./Transport.sass";
-import auto from ".././../../media/img/auto.svg";
 
 const Transport = ({ cat, title, load_capacity, description, height, capacity, transport_body, load_side, length, width,volume, handleShow }) => {
 
@@ -33,7 +34,7 @@ const Transport = ({ cat, title, load_capacity, description, height, capacity, t
                     </div>
                     <div className="transport__subscribe_values__item">
                         <div className="transport__subscribe_values__item_img">
-                            <img src={ icon } alt="иконка" />
+                            <img src={ icon2 } alt="иконка" />
                         </div>
                         <div className="transport__subscribe_values__item_text">
                             { volume } м³
@@ -41,7 +42,7 @@ const Transport = ({ cat, title, load_capacity, description, height, capacity, t
                     </div>
                     <div className="transport__subscribe_values__item margin-item">
                         <div className="transport__subscribe_values__item_img">
-                            <img src={ icon } alt="иконка" />
+                            <img src={ icon3 } alt="иконка" />
                         </div>
                         <div className="transport__subscribe_values__item_text">
                             { capacity } паллета
